68d15984b5572297e15f0be631c94af36d2b46b9
Gregory Favre <gregory.favre@gmail.com>
2013-08-22 06:31:27 +0200

    /register url will now work

--- sportfac/profiles/urls.py
+++ sportfac/profiles/urls.py
@@ -11,7 +11,8 @@ urlpatterns = patterns('',
     url(r'^children/$', ChildrenListView.as_view(), name="profiles_children"),
     url(r'^reset/$', password_reset, name="registration_reset"),
     url(r'^summary/$', BillingView.as_view(template_name="profiles/summary.html"), name="profiles_registered_activities"),
-
+    url(r'^register/$',  MyRegistrationView.as_view(wizard=False), name="registeraccount"),
+    
     (r'', include('registration.backends.simple.urls')),
     
     
