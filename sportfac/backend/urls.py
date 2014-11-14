742376c244f558f00f2098f3fcab9560e5a09bae
Gregory Favre <gregory.favre@gmail.com>
2014-11-14 14:43:43 +0100

    Added activity CRUD in backend

--- sportfac/backend/urls.py
+++ sportfac/backend/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 from .views import CourseCreateView, CourseDeleteView, CourseDetailView, \
-                   CourseListView, CourseUpdateView
+                   CourseListView, CourseUpdateView,\
+                   ActivityCreateView, ActivityDeleteView, ActivityDetailView, \
+                   ActivityListView, ActivityUpdateView
 
 courses_patterns = patterns('', 
     url(r'^$', view=CourseListView.as_view(), 
@@ -14,11 +16,25 @@ courses_patterns = patterns('',
         name='course-update'),
     url(r'^(?P<pk>\d+)/delete$', view=CourseDeleteView.as_view(),
         name='course-delete'),
-
 )
+
+activities_patterns = patterns('', 
+    url(r'^$', view=ActivityListView.as_view(), 
+        name='activity-list'),
+    url(r'^new$', view=ActivityCreateView.as_view(), 
+        name='activity-create'),
+    url(r'^(?P<pk>\d+)/$', view=ActivityDetailView.as_view(), 
+        name='activity-detail'),
+    url(r'^(?P<pk>\d+)/update$', view=ActivityUpdateView.as_view(), 
+        name='activity-update'),
+    url(r'^(?P<pk>\d+)/delete$', view=ActivityDeleteView.as_view(),
+        name='activity-delete'),
+)
+
 
 
 urlpatterns = patterns('',
     url(r'^course/', include(courses_patterns)),
+    url(r'^activity/', include(activities_patterns)),
 
 )
--- sportfac/backend/views.py
+++ sportfac/backend/views.py
@@ -4,7 +4,7 @@ from django.views.generic import CreateView, DeleteView, DetailView, \
                                  ListView, UpdateView
 
 from . import GROUP_NAME
-from activities.models import Course
+from activities.models import Course, Activity
 
 from braces.views import GroupRequiredMixin, LoginRequiredMixin
 
@@ -14,13 +14,15 @@ class BackendMixin(GroupRequiredMixin, LoginRequiredMixin):
        of sports managers group."""
     group_required = GROUP_NAME
 
+################################################################################
+# Courses
 class CourseDetailView(BackendMixin, DetailView):
     model = Course
-    template_name = 'backend/course_detail.html'
+    template_name = 'backend/course/detail.html'
 
 class CourseListView(BackendMixin, ListView):
     model = Course
-    template_name = 'backend/course_list.html'
+    template_name = 'backend/course/list.html'
    
 
 class CourseCreateView(BackendMixin, CreateView):
@@ -29,7 +31,7 @@ class CourseCreateView(BackendMixin, CreateView):
               'number_of_sessions', 'day', 'start_date', 'end_date',
               'start_time', 'end_time', 'place', 'min_participants',
               'max_participants', 'schoolyear_min', 'schoolyear_max')
-    template_name = 'backend/course_form.html'
+    template_name = 'backend/course/form.html'
 
 class CourseUpdateView(BackendMixin, UpdateView):
     model = Course
@@ -41,5 +43,33 @@ class CourseUpdateView(BackendMixin, UpdateView):
     
 class CourseDeleteView(BackendMixin, DeleteView):
     model = Course
-    template_name = 'backend/course_confirm_delete.html'
+    template_name = 'backend/course/confirm_delete.html'
     success_url = reverse_lazy('backend:course-list')
+
+
+
+################################################################################
+# Activities
+class ActivityDetailView(BackendMixin, DetailView):
+    model = Activity
+    template_name = 'backend/activity/detail.html'
+
+class ActivityListView(BackendMixin, ListView):
+    model = Activity
+    template_name = 'backend/activity/list.html'
+   
+
+class ActivityCreateView(BackendMixin, CreateView):
+    model = Activity
+    fields = ('name', 'number', 'informations', 'description')
+    template_name = 'backend/activity/form.html'
+
+class ActivityUpdateView(BackendMixin, UpdateView):
+    model = Activity
+    fields = ('name', 'number', 'informations', 'description')
+    template_name = 'backend/activity/form.html'
+    
+class ActivityDeleteView(BackendMixin, DeleteView):
+    model = Activity
+    template_name = 'backend/activity/confirm_delete.html'
+    success_url = reverse_lazy('backend:activity-list')
