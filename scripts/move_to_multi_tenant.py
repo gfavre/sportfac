8c0b8427664fd4d74413aa7a5a0e861e8e9ebd60
Gregory Favre <greg@beyondthewall.ch>
2016-04-27 12:39:50 +0200

    migration

---
+++ scripts/move_to_multi_tenant.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from constance.admin import config
+
+from backend.models import YearTenant, Domain
+
+tenant = YearTenant(
+    schema_name='public',
+    start_date=config.START_REGISTRATION,
+    end_date=config.START_REGISTRATION + timedelta(days=365)
+)
+tenant.save()
+    
+domain = Domain()
+domain.domain = settings.DEFAULT_TENANT_NAME
+domain.tenant = tenant
+domain.is_primary = True
+domain.save()
+    
+  
+tenant = YearTenant(
+    schema_name='year_2016_2017',
+    start_date=datetime(2016, 8, 1),
+    end_date=datetime(2017, 7, 31)
+)
+tenant.save()
+    
+domain = Domain()
+domain.domain = '2016-2017'
+domain.tenant = tenant
+domain.is_primary = True
+domain.save()
+    
