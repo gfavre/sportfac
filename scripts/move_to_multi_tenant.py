506773e36ad1e24a3c0d3a28f8f4f339975958dd
Gregory Favre <greg@beyondthewall.ch>
2016-04-28 12:48:31 +0200

    Models adaptation

--- scripts/move_to_multi_tenant.py
+++ scripts/move_to_multi_tenant.py
@@ -18,7 +18,30 @@ domain.tenant = tenant
 domain.is_primary = True
 domain.save()
     
-  
+
+
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from constance.admin import config
+
+from backend.models import YearTenant, Domain
+
+tenant = YearTenant(
+    schema_name='year_2015_2016',
+    start_date=datetime(2015, 8, 1),
+    end_date=datetime(2016, 7, 31)
+)
+tenant.save()
+    
+domain = Domain()
+domain.domain = '2015-2016'
+domain.tenant = tenant
+domain.is_primary = True
+domain.save()
+    
+
+
 tenant = YearTenant(
     schema_name='year_2016_2017',
     start_date=datetime(2016, 8, 1),
