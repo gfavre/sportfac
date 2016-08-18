4236afa50ae2ae6ad269e06b93877cb3683cc8bb
Gregory Favre <greg@beyondthewall.ch>
2016-08-18 16:38:11 +0200

    settings for coppet

--- sportfac/sportfac/settings/local.py
+++ sportfac/sportfac/settings/local.py
@@ -108,18 +108,18 @@ CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 
 
 TEMPLATES[0]['DIRS'] = [
-    normpath(join(SITE_ROOT, 'themes', 'nyon', 'templates')),
+    normpath(join(SITE_ROOT, 'themes', 'coppet', 'templates')),
     normpath(join(SITE_ROOT, 'templates')),
 ]
 
 STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, 'themes', 'nyon', 'static')),
+    normpath(join(SITE_ROOT, 'themes', 'coppet', 'static')),
     normpath(join(SITE_ROOT, 'static')),
 )
 
 KEPCHUP_USE_ABSENCES = True
-KEPCHUP_IMPORT_CHILDREN = True
-KEPCHUP_PREFILL_YEARS_WITH_TEACHERS = False
+KEPCHUP_IMPORT_CHILDREN = False
+KEPCHUP_PREFILL_YEARS_WITH_TEACHERS = True
 KEPCHUP_SEND_PRESENCE_LIST = True 
 KEPCHUP_SEND_COPY_CONTACT_MAIL_TO_ADMIN = True
 KEPCHUP_NO_PAYMENT = False
