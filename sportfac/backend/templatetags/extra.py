eee88ce264121c223223db716cbc52f82a1e792e
Gregory Favre <greg@beyondthewall.ch>
2016-12-20 06:57:32 +0100

    new - filter has question

--- sportfac/backend/templatetags/extra.py
+++ sportfac/backend/templatetags/extra.py
@@ -5,9 +5,19 @@ register = template.Library()
 @register.filter(is_safe=True)
 def answer_to(registration, question):
     if question.isdigit():
-        qs = registration.extra_infos.filter(key__pk=int(question), key__in=registration.course.extra.all())
+        qs = registration.extra_infos.filter(key__pk=int(question),
+                                             key__in=registration.course.extra.all())
     else:
-        qs = registration.extra_infos.filter(key__question_label=question, key__in=registration.course.extra.all())
+        qs = registration.extra_infos.filter(key__question_label=question,
+                                             key__in=registration.course.extra.all())
     if qs.count():
         return qs.last().value
     return ''
+
+@register.filter(is_safe=True)
+def has_question(registration, question):
+    if question.isdigit():
+        qs = registration.course.extra.filter(pk=int(question))
+    else:
+        qs = registration.course.extra.filter(question_label=question)
+    return qs.count() > 0
