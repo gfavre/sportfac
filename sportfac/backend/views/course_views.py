61bf6e938092ba16ecbc3f9f992ed14ee321b879
Gregory Favre <greg@beyondthewall.ch>
2017-06-26 15:44:41 +0200

    fix - courser creation

--- sportfac/backend/views/course_views.py
+++ sportfac/backend/views/course_views.py
@@ -129,7 +129,7 @@ class CourseCreateView(SuccessMessageMixin, BackendMixin, CreateView):
         initial = super(CourseCreateView, self).get_initial()
         activity = self.request.GET.get('activity', None)
         if activity:
-            activity_obj = get_object_or_404(Activity, pk=activity_id)
+            activity_obj = get_object_or_404(Activity, pk=activity)
             initial['activity'] = activity_obj
         return initial
 
