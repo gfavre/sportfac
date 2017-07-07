40f410cf55c7254525480b87e6b4f1322968af89
Gregory Favre <greg@beyondthewall.ch>
2017-07-07 17:43:39 +0200

    Add Vevey

---
+++ sportfac/sportfac/settings/vevey.py
@@ -0,0 +1,29 @@
+"""Production settings and globals."""
+
+from production import *
+
+TEMPLATES[0]['DIRS'] = [
+    normpath(join(SITE_ROOT, 'themes', 'vevey', 'templates')),
+    normpath(join(SITE_ROOT, 'templates')),
+]
+
+STATICFILES_DIRS = (
+    normpath(join(SITE_ROOT, 'themes', 'vevey', 'static')),
+    normpath(join(SITE_ROOT, 'static')),
+)
+RAVEN_CONFIG = {
+    'dsn': 'https://226a25de763049f1aa6b2405815ad74f:f3ec6c3228264c5289854488cb3bd928@sentry.evoe.wine/4',
+}
+
+
+LOGIN_URL = '/hiver/account/login/'
+LOGOUT_URL = '/hiver/account/logout/'
+
+KEPCHUP_USE_ABSENCES = True
+KEPCHUP_IMPORT_CHILDREN = False
+KEPCHUP_PREFILL_YEARS_WITH_TEACHERS = False
+KEPCHUP_SEND_PRESENCE_LIST = True
+KEPCHUP_SEND_COPY_CONTACT_MAIL_TO_ADMIN = True
+KEPCHUP_NO_PAYMENT = True
+KEPCHUP_NO_TERMS = False
+KEPCHUP_NO_SSF = True
