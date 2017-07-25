d1dee03e9fe99c36f43b7095cf694452c9ab0540
Gregory Favre <greg@beyondthewall.ch>
2017-07-25 14:04:15 +0200

    reset user passwords from admin

--- sportfac/profiles/admin.py
+++ sportfac/profiles/admin.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib import admin
-from django.contrib.auth.models import Group, User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.utils.translation import ugettext_lazy as _
 
-from .models import FamilyUser, School, SchoolYear
 from registrations.models import Child
+from .models import FamilyUser, School, SchoolYear
 
 
 class FamilyCreationForm(forms.ModelForm):
@@ -18,9 +17,9 @@ class FamilyCreationForm(forms.ModelForm):
 
     class Meta:
         model = FamilyUser
-        fields = ('email', 'first_name', 'last_name', 
-                  'address', 'zipcode', 'city', 'country', 
-                  'private_phone', 'private_phone2', 
+        fields = ('email', 'first_name', 'last_name',
+                  'address', 'zipcode', 'city', 'country',
+                  'private_phone', 'private_phone2',
                   'birth_date', 'iban', 'ahv')
 
     def clean_password2(self):
@@ -47,16 +46,15 @@ class FamilyChangeForm(forms.ModelForm):
     """
     password = ReadOnlyPasswordHashField(help_text= _("Raw passwords are not stored, so there is no way to see "
                     "this user's password, but you can change the password "
-                    "using <a href=\"password/\">this form</a>."))
+                    "using <a href=\"../password/\">this form</a>."))
 
     class Meta:
         model = FamilyUser
         fields = ('email', 'is_staff', 'is_superuser', 'groups',
-                  'first_name', 'last_name', 
-                  'address', 'zipcode', 'city', 'country', 
+                  'first_name', 'last_name',
+                  'address', 'zipcode', 'city', 'country',
                   'private_phone', 'private_phone2', 'private_phone3',
                   'birth_date', 'iban', 'ahv')
-
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
@@ -71,47 +69,40 @@ class ChildInline(admin.StackedInline):
     verbose_name = _("child")
     verbose_name_plural = _("children")
 
- 
+
 class FamilyAdmin(UserAdmin):
     # The forms to add and change user instances
     form = FamilyChangeForm
     add_form = FamilyCreationForm
 
     list_display = ('email', 'first_name', 'last_name', 'children_names', 'last_login', 'date_joined')
-    
-    #change_list_template = "admin/change_list_filter_sidebar.html"
     change_list_filter_template = "admin/filter_listing.html"
 
-    
     fieldsets = (
         (None, {'fields': ('email', 'password', 'is_staff', 'is_superuser', 'groups')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 
-                                      'address', 'zipcode', 'city', 'country', 
+        ('Personal info', {'fields': ('first_name', 'last_name',
+                                      'address', 'zipcode', 'city', 'country',
                                       'private_phone', 'private_phone2', 'private_phone3',
                                       'birth_date', 'iban', 'ahv')}),
-        
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')}
         ),
-        ('Personal info', {'fields': ('first_name', 'last_name', 
-                                      'address', 'zipcode', 'city', 'country', 
+        ('Personal info', {'fields': ('first_name', 'last_name',
+                                      'address', 'zipcode', 'city', 'country',
                                       'private_phone', 'private_phone2',
                                       'birth_date', 'iban', 'ahv')}),
     )
     search_fields = ('email', 'last_name', 'first_name',)
     ordering = ('last_name', 'first_name')
-    #filter_horizontal = ()
     inlines = [ChildInline]
-    
+
     def get_queryset(self, request):
         qs = super(FamilyAdmin, self).get_queryset(request)
-        #return qs 19
         return qs.prefetch_related('children')
-    
-    
+
 
 admin.site.register(FamilyUser, FamilyAdmin)
 admin.site.register(SchoolYear)
