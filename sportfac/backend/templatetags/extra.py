05f850ffbcbe1e47bff5a9beb15d1af5a6074e75
Gregory Favre <greg@beyondthewall.ch>
2018-01-05 18:34:34 +0100

    fix - display all questions

--- sportfac/backend/templatetags/extra.py
+++ sportfac/backend/templatetags/extra.py
@@ -7,11 +7,9 @@ register = template.Library()
 @register.filter(is_safe=True)
 def answer_to(registration, question):
     if question.isdigit():
-        qs = registration.extra_infos.filter(key__pk=int(question),
-                                             key__in=registration.course.extra.all())
+        qs = registration.extra_infos.filter(key__pk=int(question))
     else:
-        qs = registration.extra_infos.filter(key__question_label__startswith=question,
-                                             key__in=registration.course.extra.all())
+        qs = registration.extra_infos.filter(key__question_label__startswith=question)
     if qs.count():
         return qs.last().value
     return ''
