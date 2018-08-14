7b3904d8931431518516704443f0034303f362de
Gregory Favre <greg@beyondthewall.ch>
2018-08-14 16:18:11 +0200

    fix - xlrd date

--- sportfac/registrations/utils.py
+++ sportfac/registrations/utils.py
@@ -11,17 +11,22 @@ from registrations.models import Child
 
 CHILD_MANDATORY_FIELDS = (u'ID LAGAPEO', u'Nom', u'Prénom', u'Genre', u'Date de naissance', u'Nationalité', u'Langue maternelle')
 CHILD_IMPORT_TO_FIELD = {
-    u'ID LAGAPEO': 'id_lagapeo', 
-    u'Nom': 'last_name', 
-    u'Prénom': 'first_name', 
+    u'ID LAGAPEO': 'id_lagapeo',
+    u'Nom': 'last_name',
+    u'Prénom': 'first_name',
     u'Courriel 1': 'email',}
+
 
 class ChildParser:
 
-    def __init__(self):
+    def __init__(self, book=None):
+        if book
+            self.datemode = book.datemode
+        else:
+            self.datemode = 0
         self.fields_dict = {
-            u'ID LAGAPEO': ('id_lagapeo', lambda x: int(x)), 
-            u'Nom': ('last_name', lambda x: x), 
+            u'ID LAGAPEO': ('id_lagapeo', lambda x: int(x)),
+            u'Nom': ('last_name', lambda x: x),
             u'Prénom': ('first_name', lambda x: x),
             u'Genre': ('sex', self.parse_sex),
             u'Date de naissance': ('birth_date', self.parse_birth_date),
@@ -36,18 +41,23 @@ class ChildParser:
 
     def parse_birth_date(self, value):
         try:
-            return datetime.strptime(value, '%d.%m.%Y').date()
+            if isinstance(value, basestring):
+                return datetime.strptime(value, '%d.%m.%Y').date()
+            else:
+                return xlrd.xldate_as_datetime(value, self.datemode)
         except ValueError:
             return None
-    
+        except TypeError:
+            return None
+
     def parse_nationality(self, value):
         if value == 'Suisse':
             return Child.NATIONALITY.CH
         elif value == 'Liechtenstein':
             return Child.NATIONALITY.FL
         else:
-            return Child.NATIONALITY.DIV 
-    
+            return Child.NATIONALITY.DIV
+
     def parse_language(self, value):
         if value == u'Français':
             return Child.LANGUAGE.F
@@ -57,9 +67,9 @@ class ChildParser:
             return Child.LANGUAGE.D
         elif value == u'Anglais':
             return Child.LANGUAGE.E
-        
+
         return Child.LANGUAGE.F
-    
+
     def parse(self, row):
         out = {}
         for key, val in row.items():
@@ -74,21 +84,21 @@ def load_children(filelike):
         xls_book = xlrd.open_workbook(file_contents=filelike.read())
         sheet = xls_book.sheets()[0]
         header_row = sheet.row_values(0)
-                
+
         if not all(key in header_row for key in CHILD_MANDATORY_FIELDS):
             raise ValueError(_("All these fields are mandatory: %s") % unicode(CHILD_MANDATORY_FIELDS))
     except xlrd.XLRDError:
         raise ValueError(_("File format is unreadable"))
     nb_created = 0
     nb_updated = 0
-    
-    parser = ChildParser()
+
+    parser = ChildParser(xls_book)
     for i in moves.range(1, sheet.nrows):
         values = dict(zip(header_row, sheet.row_values(i)))
         parsed = parser.parse(values)
         id_lagapeo = parsed.pop('id_lagapeo')
         child, created = Child.objects.update_or_create(id_lagapeo=id_lagapeo, defaults=parsed)
-        if created: 
+        if created:
             nb_created += 1
         else:
             nb_updated += 1
