78d7b9276b3bcfc5f23d168315028e650763bda4
Gregory Favre <greg@beyondthewall.ch>
2018-09-06 15:56:40 +0200

    imp - new mails

--- sportfac/sportfac/settings/montreux.py
+++ sportfac/sportfac/settings/montreux.py
@@ -28,7 +28,7 @@ KEPCHUP_NO_TERMS = False
 KEPCHUP_CHILD_SCHOOL = True
 KEPCHUP_FICHE_SALAIRE_MONTREUX = True
 KEPCHUP_NO_SSF = True
-KEPCHUP_ADDITIONAL_INSTRUCTOR_EMAIL_DOCUMENTS = ['pdf/infos-moniteurs-printemps-2018.pdf', 'pdf/GMS_2017-2018.pdf']
+KEPCHUP_ADDITIONAL_INSTRUCTOR_EMAIL_DOCUMENTS = ['pdf/infos-moniteurs-2018.pdf', 'pdf/GMS_2018-2019.pdf']
 
 CELERYBEAT_SCHEDULE['notify-absences'] = {
         'task': 'absences.tasks.notify_absences',
