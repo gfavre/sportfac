240bdce1037ddf63f3b47f7adb5cc442beb5ad58
Gregory Favre <greg@beyondthewall.ch>
2018-10-16 13:52:18 +0200

    contact

--- sportfac/contact/forms.py
+++ sportfac/contact/forms.py
@@ -6,13 +6,14 @@ from django.conf import settings
 import floppyforms as forms
 from dynamic_preferences.registries import global_preferences_registry
 
+
 class ContactForm(forms.Form):
     name = forms.CharField(label=_("Name"),
                            widget=forms.TextInput())
     email = forms.EmailField(label=_("E-mail"),
                              widget=forms.EmailInput(attrs={'placeholder': 'john@example.com',}))
     subject = forms.CharField(label=_("Subject"),
-                           widget=forms.TextInput())
+                              widget=forms.TextInput())
 
     message = forms.CharField(label=_("Message"),
                               widget=forms.Textarea(attrs={'rows': 5}))
@@ -25,16 +26,16 @@ class ContactForm(forms.Form):
             bcc = ['%s <%s>' % (mail_tuple[0], mail_tuple[1]) for mail_tuple in settings.MANAGERS]
 
         email = EmailMessage(
-            subject = u'%s [%s - formulaire de contact]' % (
+            subject=u'%s [%s - formulaire de contact]' % (
                 self.cleaned_data['subject'],
                 global_preferences['email__SCHOOL_NAME'].decode('utf-8')),
-            body = u'%s <%s> a utilisé le formulaire de contact.\n\n %s' % (
+            body=u'%s <%s> a utilisé le formulaire de contact.\n\n %s' % (
                 self.cleaned_data['name'],
                 self.cleaned_data['email'],
                 self.cleaned_data['message']),
-            from_email = global_preferences['email__FROM_MAIL'].decode('utf-8'),
-            to = [global_preferences['email__CONTACT_MAIL'].decode('utf-8')],
-            bcc = bcc,
-            reply_to = ['%s <%s>' % (self.cleaned_data['name'], self.cleaned_data['email'])]
+            from_email=global_preferences['email__FROM_MAIL'].decode('utf-8'),
+            to=[global_preferences['email__CONTACT_MAIL'].decode('utf-8')],
+            bcc=bcc,
+            reply_to=['%s <%s>' % (self.cleaned_data['name'], self.cleaned_data['email'])]
         )
         email.send(fail_silently=fail_silently)
