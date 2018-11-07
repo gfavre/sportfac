b3a2919fe54837cf1f99c60c70e32d1d67c57688
Gregory Favre <greg@beyondthewall.ch>
2018-11-07 06:53:33 +0100

    ...

--- sportfac/registrations/tasks.py
+++ sportfac/registrations/tasks.py
@@ -11,42 +11,45 @@ from backend.models import Domain
 from mailer.tasks import send_mail
 from profiles.models import FamilyUser
 from registrations.models import Bill
-from sportfac.decorators import respects_language
 
 
-@shared_task(bind=True, max_retries=2)
-@respects_language
-def send_confirmation(self, user_pk, bill_pk):
-    current_domain = Domain.objects.filter(is_current=True).first()
-    connection.set_tenant(current_domain.tenant)
-
-    global_preferences = global_preferences_registry.manager()
-
-    user = FamilyUser.objects.get(pk=user_pk)
+@shared_task(bind=True, max_retries=3)
+def send_confirmation(self, user_pk, bill_pk, language=settings.LANGUAGE_CODE):
+    current_lang = translation.get_language()
     try:
-        bill = Bill.objects.get(pk=bill_pk)
-    except Bill.DoesNotExist:
-        self.retry(countdown=5 ** self.request.retries)
-        return 
-    registrations = bill.registrations.all()
-    current_site = Site.objects.get_current()
+        translation.activate(language)
+        current_domain = Domain.objects.filter(is_current=True).first()
+        connection.set_tenant(current_domain.tenant)
 
-    context = {
-        'user': user,
-        'registrations': registrations,
-        'bill': bill,
-        'iban': global_preferences['payment__IBAN'],
-        'signature': global_preferences['email__SIGNATURE'],
-        'payment': not settings.KEPCHUP_NO_PAYMENT,
-        'site_name': current_site.name,
-        'site_url': settings.DEBUG and 'http://' + current_site.domain or 'https://' + current_site.domain
-    }
-    subject = render_to_string('registrations/confirmation_mail_subject.txt', context=context)
+        global_preferences = global_preferences_registry.manager()
 
-    body = render_to_string('registrations/confirmation_mail.txt', context=context)
-    send_mail.delay(
-        subject=subject, message=body,
-        from_mail=global_preferences['email__FROM_MAIL'],
-        recipients=[user.get_email_string()],
-        reply_to=[global_preferences['email__REPLY_TO_MAIL']]
-    )
+        user = FamilyUser.objects.get(pk=user_pk)
+        try:
+            bill = Bill.objects.get(pk=bill_pk)
+        except Bill.DoesNotExist:
+            self.retry(countdown=5 ** self.request.retries)
+            return
+        registrations = bill.registrations.all()
+        current_site = Site.objects.get_current()
+
+        context = {
+            'user': user,
+            'registrations': registrations,
+            'bill': bill,
+            'iban': global_preferences['payment__IBAN'],
+            'signature': global_preferences['email__SIGNATURE'],
+            'payment': not settings.KEPCHUP_NO_PAYMENT,
+            'site_name': current_site.name,
+            'site_url': settings.DEBUG and 'http://' + current_site.domain or 'https://' + current_site.domain
+        }
+        subject = render_to_string('registrations/confirmation_mail_subject.txt', context=context)
+
+        body = render_to_string('registrations/confirmation_mail.txt', context=context)
+        send_mail.delay(
+            subject=subject, message=body,
+            from_mail=global_preferences['email__FROM_MAIL'],
+            recipients=[user.get_email_string()],
+            reply_to=[global_preferences['email__REPLY_TO_MAIL']]
+        )
+    finally:
+        translation.activate(current_lang)
