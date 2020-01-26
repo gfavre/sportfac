c44a58ea2619bbfe36e90400b5c3ae611b877f12
Gregory Favre <greg@beyondthewall.ch>
2020-01-26 16:31:28 +0100

    new version

--- scripts/clean_absences.py
+++ scripts/clean_absences.py
@@ -5,12 +5,13 @@ from absences.models import Session, Absence
 from registrations.models import Registration
 from registrations.models import Child
 
+c=0
 for course in Course.objects.all():
     children = [r.child for r in course.participants.all()]
-    for session in course.sessions.all():
+    for session in course.sessions.filter(date__gte=datetime.date.today()):
         for absence in list(session.absences.all()):
             if absence.child not in children:
-                print(u'c:{} - delete absence'.format(course))
+                c+=1
                 absence.delete()
 
 
