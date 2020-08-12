371020eb5a0b35c6f72451e30fce0fe532780d70
Gregory Favre <greg@beyondthewall.ch>
2020-08-12 23:19:16 +0200

    fix time

--- sportfac/sportfac/settings/montreux.py
+++ sportfac/sportfac/settings/montreux.py
@@ -65,5 +65,5 @@ CELERYBEAT_SCHEDULE['notify-absences'] = {
 }
 CELERYBEAT_SCHEDULE['sync_from_master'] = {
         'task': 'profiles.tasks.sync_from_master',
-        'schedule': crontab(minute='*/1'),
+        'schedule': crontab(minute='*/10'),
 }
