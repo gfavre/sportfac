b5ea46b7994f5c81e8650f6f7cce2159ca9b47b4
Gregory Favre <greg@beyondthewall.ch>
2020-11-13 15:09:05 +0100

    fix - excel export

--- sportfac/appointments/resources.py
+++ sportfac/appointments/resources.py
@@ -1,35 +1,37 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
 from import_export import resources, fields
 
 from .models import Appointment
 
 
 class AppointmentResource(resources.ModelResource):
-    child = fields.Field(column_name=_("Child"))
+    child_first = fields.Field(column_name=_("First name"), attribute='child__first_name')
+    child_last = fields.Field(column_name=_("Last name"), attribute='child__last_name')
 
     date = fields.Field(column_name=_("Date"))
     time = fields.Field(column_name=_("Time"))
     ssf = fields.Field(column_name=_("SSF number"), attribute='child__id_lagapeo')
     has_registrations = fields.Field(column_name=_("Has registrations"))
     family = fields.Field(column_name=_("Family"))
-    email = fields.Field(column_name=_("Email"))
-
-    phone_number = fields.Field(column_name=_("Phone number"))
-
+    email = fields.Field(column_name=_("Email"), attribute='email')
+    phone_number = fields.Field(column_name=_("Phone number"), attribute='phone_number')
 
     class Meta:
         model = Appointment
-        fields = ('date', 'time', 'child', 'ssf', 'has_registrations', 'family', 'email', 'phone_number')
+        fields = ('date', 'time', 'child_first', 'child_last', 'ssf', 'has_registrations', 'family', 'email',
+                  'phone_number')
         export_order = fields
 
     # noinspection PyMethodMayBeStatic
     def dehydrate_date(self, appointment):
-        return appointment.slot.start.date()
+        return timezone.localtime(appointment.slot.start).date()
 
     # noinspection PyMethodMayBeStatic
     def dehydrate_time(self, appointment):
-        return appointment.slot.start.time()
+        return timezone.localtime(appointment.slot.start).time()
 
     # noinspection PyMethodMayBeStatic
     def dehydrate_child(self, appointment):
