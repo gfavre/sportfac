8be9e9e2a7448e62d6d63c97ce5911ea318f32eb
Gregory Favre <greg@beyondthewall.ch>
2021-10-28 16:24:36 +0200

    fix - preferences

--- update.sh
+++ update.sh
@@ -7,10 +7,15 @@ BEAT="${PROJECT}_beat"
 supervisorctl stop $PROJECT
 supervisorctl stop $WORKER
 supervisorctl stop $BEAT
+
 git pull
+
 pip install -r requirements/production.txt
+
 python sportfac/manage.py migrate
 python sportfac/manage.py collectstatic --noinput
+python sportfac/manage.py checkpreferences
+
 supervisorctl start $PROJECT
 supervisorctl start $WORKER
 supervisorctl start $BEAT
