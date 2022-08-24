b814216b4af9e3d9abb06cc76caee08450dcecf0
Gregory Favre <greg@beyondthewall.ch>
2022-08-24 11:35:35 +0200

    waiting slot admin

--- sportfac/waiting_slots/admin.py
+++ sportfac/waiting_slots/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import WaitingSlot
 
-# Register your models here.
+
+@admin.register(WaitingSlot)
+class WaitingSlotAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    list_display = ('child', 'course', 'created')
+    list_filter = ('course__activity', 'created')
+    raw_id_fields = ('child', 'course')
+    search_fields = ('course__activity__name', 'course__identifier', 'child__first_name', 'child__last_name')
+    ordering = ('course__activity__name', 'course__number', 'created')
