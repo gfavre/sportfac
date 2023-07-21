9384ab7e9f03a98303f45bb0ba6b973e04a5ad2a
Gregory Favre <greg@beyondthewall.ch>
2023-07-21 17:10:39 +0200

    - More fields to detail export of payroll

--- sportfac/payroll/utils.py
+++ sportfac/payroll/utils.py
@@ -15,11 +15,27 @@ def get_payroll_csv(payroll_obj, filelike):
         qs_kwargs["export_date__isnull"] = True
     sessions = Session.objects.filter(**qs_kwargs).values_list("course", "instructor")
     count = {}
-    for (course_id, instructor_id) in sessions:
+    for course_id, instructor_id in sessions:
         count[(course_id, instructor_id)] = count.get((course_id, instructor_id), 0) + 1
     writer = ExcelWriter(
         filelike,
     )
+    if payroll_obj.add_details:
+        heading = [
+            "Identifiant moniteur",
+            "Code de fonction",
+            "Nombre d'heures",
+            "Date de début",
+            "Date de fin",
+            "Moniteur",
+            "Activité",
+            "Course",
+            "Durée",
+            "Fonction",
+            "Tarif horaire",
+            "Effectif moyen",
+        ]
+        writer.writerow(heading)
     for course_instructor in CoursesInstructors.objects.all():
         if (course_instructor.course_id, course_instructor.instructor_id) not in count:
             continue
@@ -31,10 +47,7 @@ def get_payroll_csv(payroll_obj, filelike):
         course_instructor.exported_count = nb_rate
         if course_instructor.function.is_hourly:
             duration = course_instructor.course.duration
-            nb_hours = (
-                Decimal(duration.seconds / 3600.0 + duration.days * 24)
-                * course_instructor.exported_count
-            )
+            nb_hours = Decimal(duration.seconds / 3600.0 + duration.days * 24) * course_instructor.exported_count
         elif course_instructor.function.is_daily:
             nb_hours = course_instructor.exported_count
         else:
@@ -48,9 +61,19 @@ def get_payroll_csv(payroll_obj, filelike):
             payroll_obj.end.strftime(settings.SWISS_DATE_SHORT),
         ]
         if payroll_obj.add_details:
+            course_sessions = course_instructor.course.sessions.filter(**qs_kwargs)
+            total_presentees = sum([session.presentees_nb() for session in course_sessions])
+            nb_sessions = course_sessions.count()
+            avg_presentees = round(float(total_presentees) / max(nb_sessions, 1), 1)
+
             line += [
                 course_instructor.instructor.full_name,
+                course_instructor.course.activity.name,
+                course_instructor.course.name,
+                course_instructor.course.duration,
                 course_instructor.function.name,
+                course_instructor.function.rate,
+                avg_presentees,
             ]
         writer.writerow(line)
     if payroll_obj.set_as_exported:
