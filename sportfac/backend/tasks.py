d40c54e4b3a3c6c7163973f0f1d3ddd2f6c8975e
Gregory Favre <greg@beyondthewall.ch>
2023-08-15 16:50:58 +0200

    fix - import children

--- sportfac/backend/tasks.py
+++ sportfac/backend/tasks.py
@@ -3,14 +3,10 @@ from datetime import datetime
 from tempfile import NamedTemporaryFile
 
 from django.conf import settings
-from django.contrib import messages
 from django.contrib.sessions.models import Session
 from django.core.management import call_command
 from django.db import connection, transaction
-from django.urls import reverse
 from django.utils import timezone
-from django.utils.html import format_html, mark_safe
-from django.utils.translation import gettext as _
 
 from activities.models import Course
 from celery import shared_task
@@ -26,35 +22,10 @@ from .models import Domain, YearTenant
 
 logger = get_task_logger(__name__)
 
-"""
-from django.db import connection, transaction
-from django.utils.timezone import now
-
-from backend.models import YearTenant, Domain
-
-start=end=now()
-
-
-destination_tenant, created = YearTenant.objects.get_or_create(
-        schema_name='period_{}_{}'.format(
-            start.strftime('%Y%m%d'),
-            end.strftime('%Y%m%d')
-        ),
-        defaults={
-            'start_date': start,
-            'end_date': end,
-            'status': YearTenant.STATUS.creating
-        }
-    )
-destination_tenant.create_schema(check_if_exists=True)
-"""
-
 
 @shared_task
 @respects_language
-def create_tenant(
-    start, end, copy_activities_from_id=None, copy_children_from_id=None, user_id=None
-):
+def create_tenant(start, end, copy_activities_from_id=None, copy_children_from_id=None, user_id=None):
     start = datetime.strptime(start, "%Y-%m-%d").date()
     end = datetime.strptime(end, "%Y-%m-%d").date()
     connection.set_schema_to_public()
@@ -71,7 +42,7 @@ def create_tenant(
         )
         Domain.objects.get_or_create(
             tenant=destination_tenant,
-            domain="%s-%s" % (start.strftime("%Y%m%d"), end.strftime("%Y%m%d")),
+            domain="{}-{}".format(start.strftime("%Y%m%d"), end.strftime("%Y%m%d")),
         )
     try:
         user = FamilyUser.objects.get(pk=user_id)
@@ -118,9 +89,7 @@ def create_tenant(
             logger.debug("Set all courses to not up-to-date in destination")
 
         except YearTenant.DoesNotExist:
-            logger.warning(
-                "Year tenant {} (source) does not exist".format(copy_activities_from_id)
-            )
+            logger.warning(f"Year tenant {copy_activities_from_id} (source) does not exist")
 
     if copy_children_from_id:
         logger.info("Beginning copy of children")
@@ -149,7 +118,7 @@ def create_tenant(
             f = NamedTemporaryFile(suffix=".json", delete=False)
             call_command("dumpdata", "registrations.Child", output=f.name)
             f.close()
-            logger.debug("Dumped children from source: {}".format(f.name))
+            logger.debug(f"Dumped children from source: {f.name}")
 
             connection.set_tenant(destination_tenant)
             call_command("loaddata", f.name)
@@ -165,7 +134,7 @@ def create_tenant(
             logger.debug("Set all children to not up-to-date in destination")
 
         except YearTenant.DoesNotExist:
-            logger.warning("Year tenant {} (source) does not exist".format(copy_children_from_id))
+            logger.warning(f"Year tenant {copy_children_from_id} (source) does not exist")
 
     if settings.KEPCHUP_ENABLE_PAYROLLS:
         try:
@@ -187,9 +156,7 @@ def create_tenant(
                 )
             )
         except YearTenant.DoesNotExist:
-            logger.warning(
-                "Year tenant {} (source) does not exist".format(copy_activities_from_id)
-            )
+            logger.warning(f"Year tenant {copy_activities_from_id} (source) does not exist")
 
     destination_tenant.status = YearTenant.STATUS.ready
     destination_tenant.save()
@@ -198,17 +165,17 @@ def create_tenant(
             destination_tenant.start_date.isoformat(), destination_tenant.end_date.isoformat()
         )
     )
-    if user:
-        msg = _(
-            "The period %(start)s-%(end)s is ready to be used. "
-            "You can preview it at the %(link_start)speriod management interface%(link_end)s."
-        )
-        params = {
-            "start": destination_tenant.start_date.isoformat(),
-            "end": destination_tenant.end_date.isoformat(),
-            "link_start": format_html('<a href="{}">', reverse("backend:year-list")),
-            "link_end": format_html("</a>"),
-        }
+    # if user:
+    #     msg = _(
+    #         "The period %(start)s-%(end)s is ready to be used. "
+    #         "You can preview it at the %(link_start)speriod management interface%(link_end)s."
+    #     )
+    #     params = {
+    #         "start": destination_tenant.start_date.isoformat(),
+    #         "end": destination_tenant.end_date.isoformat(),
+    #         "link_start": format_html('<a href="{}">', reverse("backend:year-list")),
+    #         "link_end": format_html("</a>"),
+    #     }
 
 
 @shared_task
@@ -218,9 +185,7 @@ def update_current_tenant():
     now = timezone.now()
     current_domain = Domain.objects.filter(is_current=True).first()
     possible_new_tenants = (
-        YearTenant.objects.filter(
-            start_date__lte=now, end_date__gte=now, status=YearTenant.STATUS.ready
-        )
+        YearTenant.objects.filter(start_date__lte=now, end_date__gte=now, status=YearTenant.STATUS.ready)
         .exclude(domains=current_domain)
         .order_by("start_date", "end_date")
     )
@@ -237,13 +202,13 @@ def update_current_tenant():
         # log out everyone
         Session.objects.all().delete()
 
-        for user in FamilyUser.objects.filter(is_active=True, is_manager=True):
-            msg = _("The active period has been automatically changed to %(start)s - %(end)s")
-            params = {
-                "start": new_domain.tenant.start_date.isoformat(),
-                "end": new_domain.tenant.end_date.isoformat(),
-            }
-            # messages.info(user, msg % params)
+        # for user in FamilyUser.objects.filter(is_active=True, is_manager=True):
+        #     msg = _("The active period has been automatically changed to %(start)s - %(end)s")
+        #     params = {
+        #         "start": new_domain.tenant.start_date.isoformat(),
+        #         "end": new_domain.tenant.end_date.isoformat(),
+        #     }
+        #     # messages.info(user, msg % params)
 
         connection.set_tenant(new_domain.tenant)
 
@@ -253,20 +218,20 @@ def update_current_tenant():
 def import_children(filepath, tenant_id, user_id=None):
     tenant = YearTenant.objects.get(pk=tenant_id)
     connection.set_tenant(tenant)
-    with open(filepath) as filelike:
+    with open(filepath, "rb") as filelike:
         try:
             (nb_created, nb_updated) = load_children(filelike)
-            status = messages.constants.SUCCESS
-            message = _(
-                "Children import successful. "
-                "%(added)s children have been added, %(updated)s have been updated"
-            ) % {"added": nb_created, "updated": nb_updated}
+            # status = messages.constants.SUCCESS
+            # message = _(
+            #     "Children import successful. " "%(added)s children have been added, %(updated)s have been updated"
+            # ) % {"added": nb_created, "updated": nb_updated}
         except ValueError as err:
-            status = messages.constants.ERROR
-            message = err.message
+            # status = messages.constants.ERROR
+            # message = err
+            logger.exception("Error while importing children", exc_info=err)
 
-    try:
-        user = FamilyUser.objects.get(pk=user_id)
-        # message_user(user, message, status)
-    except FamilyUser.DoesNotExist:
-        pass
+    # try:
+    #     user = FamilyUser.objects.get(pk=user_id)
+    #     # message_user(user, message, status)
+    # except FamilyUser.DoesNotExist:
+    #     pass
--- sportfac/registrations/utils.py
+++ sportfac/registrations/utils.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 from django.utils.translation import gettext as _
 
-import xlrd
+from openpyxl import load_workbook
+from openpyxl.utils.datetime import from_excel
 from profiles.models import School, SchoolYear
 from registrations.models import Child
 
@@ -12,13 +13,6 @@ from registrations.models import Child
 logger = logging.getLogger(__name__)
 
 
-CHILD_MANDATORY_FIELDS = (
-    "ID LAGAPEO",
-    "Nom",
-    "Prénom",
-    "Genre",
-    "Date de naissance",
-)
 CHILD_MANDATORY_FIELDS = (
     "id_lagapeo",
     "first_name",
@@ -32,7 +26,7 @@ CORRESPONDANCE_DICT = {
     "last_name": ["Nom", "ELEVE_NOM"],
     "birth_date": ["Date de naissance", "ELEVE_DATE_NAISS"],
     "sex": ["Genre", "ELEVE_GENRE"],
-    "school_year": ["Année", "ANNEE", "ELEVE_ANNEE_SCOL"],
+    "school_year": ["Année", "ANNEE", "ELEVE_ANNEE_SCOL", "Année2"],
     "nationality": ["Nationalité"],
     "language": ["Langue maternelle", "LANGUE MATERNELLE"],
     "school": ["Etablissement", "ETABLISSEMENT", "ETABLISSEMENT_NOM"],
@@ -46,18 +40,13 @@ for k, v in CORRESPONDANCE_DICT.items():
 
 
 class ChildParser:
-    def __init__(self, book=None):
-        self.schoolyears = dict([(year.year, year) for year in SchoolYear.objects.all()])
-        self.schools = dict([(school.code, school) for school in School.objects.all()])
-        if book:
-            self.datemode = book.datemode
-        else:
-            self.datemode = 0
-
+    def __init__(self):
+        self.schoolyears = {year.year: year for year in SchoolYear.objects.all()}
+        self.schools = {school.code: school for school in School.objects.all()}
         self.fields_dict = {
-            "id_lagapeo": lambda x: int(x),
-            "first_name": lambda x: x,
-            "last_name": lambda x: x,
+            "id_lagapeo": lambda id_lagapeo: int(id_lagapeo),
+            "first_name": lambda first_name: first_name,
+            "last_name": lambda last_name: last_name,
             "birth_date": self.parse_birth_date,
             "sex": self.parse_sex,
             "nationality": self.parse_nationality,
@@ -66,64 +55,46 @@ class ChildParser:
             "school": self.parse_school,
             "is_blacklisted": self.parse_blacklist,
         }
-        # self.fields_dict = {
-        #     u'ID LAGAPEO': ('id_lagapeo', lambda x: int(x)),
-        #     u'ELEVE_ID_PERS': ('id_lagapeo', lambda x: int(x)),
-        #     u'Nom': ('last_name', lambda x: x),
-        #     u'Prénom': ('first_name', lambda x: x),
-        #     u'Genre': ('sex', self.parse_sex),
-        #     u'Date de naissance': ('birth_date', self.parse_birth_date),
-        #     u'Nationalité': ('nationality', self.parse_nationality),
-        #     u'Langue maternelle': ('language', self.parse_language),
-        #     u'LANGUE MATERNELLE': ('language', self.parse_language),
-        #     u'Année': ('school_year', self.parse_school_year),
-        #     u'ANNEE': ('school_year', self.parse_school_year),
-        #     u'Etablissement': ('school', self.parse_school),
-        #     u'ETABLISSEMENT': ('school', self.parse_school),
-        #     u'Blacklist': ('is_blacklisted', self.parse_blacklist),
-        #     u'BLACKLIST': ('is_blacklisted', self.parse_blacklist),
-        #
-        # }
 
-    def parse_blacklist(self, value):
+    @staticmethod
+    def parse_blacklist(value):
         if not value or value in (0, "0", "FALSE"):
             return False
         return True
 
-    def parse_sex(self, value):
+    @staticmethod
+    def parse_sex(value):
         if value == "G":
             return Child.SEX.M
         return Child.SEX.F
 
-    def parse_birth_date(self, value):
+    @staticmethod
+    def parse_birth_date(value):
         try:
             if isinstance(value, str):
                 return datetime.strptime(value, "%d.%m.%Y").date()
-            else:
-                return xlrd.xldate_as_datetime(value, self.datemode)
-        except ValueError:
-            return None
-        except TypeError:
+            return from_excel(value)
+        except (ValueError, TypeError):
             return None
 
-    def parse_nationality(self, value):
+    @staticmethod
+    def parse_nationality(value):
         if value in ("Suisse", "CH"):
             return Child.NATIONALITY.CH
-        elif value == "Liechtenstein":
+        if value == "Liechtenstein":
             return Child.NATIONALITY.FL
-        else:
-            return Child.NATIONALITY.DIV
+        return Child.NATIONALITY.DIV
 
-    def parse_language(self, value):
+    @staticmethod
+    def parse_language(value):
         if value in ("Français", "F"):
             return Child.LANGUAGE.F
-        elif value == "Italien":
+        if value == "Italien":
             return Child.LANGUAGE.I
-        elif value == "Allemand":
+        if value == "Allemand":
             return Child.LANGUAGE.D
-        elif value == "Anglais":
+        if value == "Anglais":
             return Child.LANGUAGE.E
-
         return Child.LANGUAGE.F
 
     def parse_school(self, value):
@@ -134,24 +105,19 @@ class ChildParser:
             try:
                 match = re.match(r"\s*(\d+)\s?\w*.*", value)
                 if not match:
-                    logger.debug("Year not parsed: {}".format(value))
+                    logger.debug(f"Year not parsed: {value}")
                     return None
                 value = int(match.group(1))
             except TypeError:
-                print("typeerror")
                 return self.schoolyears.get(value, None)
-            except ValueError:
-                print("valueerror")
-                return None
-            except IndexError:
-                print("indexerror")
+            except (ValueError, IndexError):
                 return None
         elif isinstance(value, float):
             value = int(value)
 
         year = self.schoolyears.get(value, None)
         if not year:
-            logger.debug("no corresponding year found: {}".format(value))
+            logger.debug(f"no corresponding year found: {value}")
         return year
 
     def parse(self, row):
@@ -159,68 +125,44 @@ class ChildParser:
         for key, val in row.items():
             translitterated_key = col_name_to_field.get(key, None)
             try:
-                translitterated_value = self.fields_dict.get(
-                    translitterated_key, lambda notfound: None
-                )(val)
+                translitterated_value = self.fields_dict.get(translitterated_key, lambda notfound: None)(val)
                 if translitterated_value:
                     out[translitterated_key] = translitterated_value
             except Exception as exc:
-                logger.warning("{}: Could not parse key={}, value={}".format(exc, key, val))
+                logger.warning(f"{exc}: Could not parse key={key}, value={val}")
                 continue
         return out
 
 
 def load_children(filelike):
     try:
-        xls_book = xlrd.open_workbook(file_contents=filelike.read())
-        sheet = xls_book.sheets()[0]
-        header_row = sheet.row_values(0)
+        xls_book = load_workbook(filelike)
+        sheet = xls_book.active
+        header_row = [cell.value for cell in sheet[1]]
         for field in CHILD_MANDATORY_FIELDS:
             if not any(set(CORRESPONDANCE_DICT[field]).intersection(set(header_row))):
-                raise ValueError("Missing mandatory field: {}".format(field))
+                raise ValueError(f"Missing mandatory field: {field}")
 
-    except xlrd.XLRDError:
-        raise ValueError(_("File format is unreadable"))
+    except (ValueError, KeyError) as exc:
+        raise ValueError(_("File format is unreadable")) from exc
     nb_created = 0
     nb_updated = 0
-    parser = ChildParser(xls_book)
-    for i in range(1, sheet.nrows):
-        values = dict(zip(header_row, sheet.row_values(i)))
+    parser = ChildParser()
+    for row_number, row in enumerate(sheet.iter_rows(min_row=2, values_only=True)):
+        real_row_number = row_number + 2
+        values = dict(zip(header_row, row))
         try:
             parsed = parser.parse(values)
         except Exception as exc:
-            logger.warning("{}: Could not parse row={}, values={}".format(exc, i, values))
+            logger.warning(f"{exc}: Could not parse row={real_row_number}, values={values}")
             continue
         id_lagapeo = parsed.pop("id_lagapeo")
         if not parsed.get("birth_date"):
-            logger.warning("{}: Could not add, missing birth date".format(id_lagapeo))
+            logger.warning(f"{id_lagapeo}: Could not add, missing birth date")
             continue
-
         child, created = Child.objects.update_or_create(id_lagapeo=id_lagapeo, defaults=parsed)
         if created:
             nb_created += 1
         else:
             nb_updated += 1
     return nb_created, nb_updated
-
-
-"""
-import re
-
-from django.utils.translation import gettext as _
-from django.utils.six import moves
-
-import xlrd
-
-from registrations.utils import ChildParser
-
-f = open('/Users/grfavre/Desktop/exportExcel_ssf.xlsx')
-xls_book = xlrd.open_workbook(file_contents=f.read())
-sheet = xls_book.sheets()[0]
-header_row = sheet.row_values(0)
-parser = ChildParser()
-for i in moves.range(1, sheet.nrows):
-    values = dict(zip(header_row, sheet.row_values(i)))
-    print parser.parse(values)
-
-"""
