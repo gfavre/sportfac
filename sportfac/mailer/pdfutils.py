34264a11e2788b478e86147f97e4cb82a6e1d1e6
Gregory Favre <greg@beyondthewall.ch>
2023-08-24 10:07:16 +0200

    Fix - PDF headings and missing phone number issue

--- sportfac/mailer/pdfutils.py
+++ sportfac/mailer/pdfutils.py
@@ -5,15 +5,13 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 from django.core.exceptions import ImproperlyConfigured
 from django.template import loader
-from django.utils.encoding import smart_text
+from django.utils.encoding import smart_str
 from django.utils.translation import activate
 
 import pypdftk
 import requests
-import six
 from backend.dynamic_preferences_registry import global_preferences_registry
 from sekizai.context import SekizaiContext
-from six.moves import range
 
 from sportfac.context_processors import kepchup_context
 
@@ -36,9 +34,9 @@ def get_ssf_decompte_heures(course, instructor):
         "Nom": instructor.last_name,
         "Prénom": instructor.first_name,
         "Adresse": instructor.address,
-        "NPA/Localité": "%s %s" % (instructor.zipcode, instructor.city),
+        "NPA/Localité": f"{instructor.zipcode} {instructor.city}",
         "Naissance": instructor.birth_date and instructor.birth_date.strftime("%d/%m/%Y") or "",
-        "Tel portable": instructor.best_phone.as_national,
+        "Tel portable": instructor.best_phone and instructor.best_phone.as_national or "",
         "IBAN": instructor.iban,
         "Banque/CCP": instructor.bank_name,
         "Formation  MEP diplômé": instructor.is_mep and "On",
@@ -57,23 +55,11 @@ def get_ssf_decompte_heures(course, instructor):
         return pdf_file
 
 
-"""
-from mailer.pdfutils import get_ssf_decompte_heures
-from activities.models import  Course
-course= Course.objects.filter(instructors__isnull=False).first()
-course = Course.objects.get(pk=38)
-instructor=course.instructors.first()
-pdf = get_ssf_decompte_heures(course, instructor)
-import shutil
-shutil.move(pdf, '/Users/grfavre/Desktop/filled.pdf')
-"""
-
-
-class FakeRequest(object):
+class FakeRequest:
     pass
 
 
-class PDFRenderer(object):
+class PDFRenderer:
     message_template = None
     is_landscape = False
 
@@ -100,10 +86,9 @@ class PDFRenderer(object):
         """Accepts a template object, path-to-template or list of paths"""
         if isinstance(template, (list, tuple)):
             return loader.select_template(template)
-        elif isinstance(template, six.string_types):
+        if isinstance(template, str):
             return loader.get_template(template)
-        else:
-            return template
+        return template
 
     def get_message_template(self):
         if self.message_template is None:
@@ -111,8 +96,7 @@ class PDFRenderer(object):
                 "PDFRenderer requires either a definition of "
                 "'message_template' or an implementation of 'get_message_template'"
             )
-        else:
-            return self.resolve_template(self.message_template)
+        return self.resolve_template(self.message_template)
 
     def get_content(self, template_name):
         initial_static_url = settings.STATIC_URL
@@ -125,13 +109,13 @@ class PDFRenderer(object):
         if self.fake_request:
             activate(settings.LANGUAGE_CODE)
             template = self.resolve_template(template_name)
-            content = smart_text(template.render(self.context))
+            content = smart_str(template.render(self.context))
         else:
             content = loader.render_to_string(
                 template_name=self.message_template, context=self.context, request=self.request
             )
         settings.STATIC_URL = initial_static_url
-        return content
+        return content  # noqa: R504
 
     def render_to_pdf(self, output):
         """output: filelike object"""
@@ -160,7 +144,7 @@ class PDFRenderer(object):
         )
 
         pdf = requests.post(
-            "https://PhantomJsCloud.com/api/browser/v2/{}/".format(settings.PHANTOMJSCLOUD_APIKEY),
+            f"https://PhantomJsCloud.com/api/browser/v2/{settings.PHANTOMJSCLOUD_APIKEY}/",
             payload,
         )
         f = open(output, "wb")
@@ -176,7 +160,7 @@ class CourseParticipantsPresence(PDFRenderer):
     message_template = "mailer/pdf_participants_presence.html"
 
     def __init__(self, context_data):
-        super(CourseParticipantsPresence, self).__init__(context_data)
+        super().__init__(context_data)
         course = context_data["course"]
         self.context["sessions"] = list(range(0, course.number_of_sessions))
 
