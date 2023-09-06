b22b00fcf4d9984a0b9cf0f04d5b5caff853cc2d
Gregory Favre <greg@beyondthewall.ch>
2023-09-06 18:29:11 +0200

    fix - registration extra

--- sportfac/registrations/templatetags/registrations.py
+++ sportfac/registrations/templatetags/registrations.py
@@ -13,10 +13,13 @@ def get_extra_info_td(registration, question):
         response = registration.extra_infos.get(key=question)
     except ExtraInfo.DoesNotExist:
         return mark_safe("<td></td>")
+    except ExtraInfo.MultipleObjectsReturned:
+        response = registration.extra_infos.filter(key=question).last()
+
     formatted_response = response.value
     if question.type == "B":
         if response.value == "1":
             formatted_response = """<i class="icon-ok-circled text-success"></i>"""
         else:
             formatted_response = """<i class="icon-cancel-circled text-danger"></i>"""
-    return mark_safe("""<td data-order="{}">{}</td>""".format(response.value, formatted_response))
+    return mark_safe(f"""<td data-order="{response.value}">{formatted_response}</td>""")
