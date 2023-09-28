24cd3ebfdfdbacdffe8e43ce3f82590378d1491d
Gregory Favre <greg@beyondthewall.ch>
2023-09-28 10:23:38 +0200

    add static ssf terms

--- sportfac/sportfac/settings/local.py
+++ sportfac/sportfac/settings/local.py
@@ -100,12 +100,12 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # END EMAIL CONFIGURATION
 
 TEMPLATES[0]["DIRS"] = [  # noqa: F405
-    normpath(join(SITE_ROOT, "themes", "montreux", "templates")),  # noqa: F405
+    normpath(join(SITE_ROOT, "themes", "latourdepeilz", "templates")),  # noqa: F405
     normpath(join(SITE_ROOT, "templates")),  # noqa: F405
 ]
 
 STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, "themes", "montreux", "static")),  # noqa: F405
+    normpath(join(SITE_ROOT, "themes", "latourdepeilz", "static")),  # noqa: F405
     normpath(join(SITE_ROOT, "static")),  # noqa: F405
 )
 COMPRESS_ENABLED = True
