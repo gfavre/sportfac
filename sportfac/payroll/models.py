12546de91906489f3b3a830eee733aaf9d5f2b8a
Gregory Favre <greg@beyondthewall.ch>
2023-10-04 16:27:35 +0200

    fix - str

--- sportfac/payroll/models.py
+++ sportfac/payroll/models.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from io import StringIO
 
 from django.core.files.base import ContentFile
@@ -12,12 +11,9 @@ from sportfac.models import TimeStampedModel
 
 
 class Function(TimeStampedModel):
-
     code = models.CharField(_("Function code"), max_length=30, unique=True)
     name = models.CharField(_("Function name"), max_length=100)
-    rate = models.DecimalField(
-        verbose_name=_("Rate"), max_digits=10, decimal_places=2, blank=True, null=True
-    )
+    rate = models.DecimalField(verbose_name=_("Rate"), max_digits=10, decimal_places=2, blank=True, null=True)
     rate_mode = models.CharField(verbose_name=_("Rate mode"), max_length=20, choices=RATE_MODES)
 
     class Meta:
@@ -40,17 +36,12 @@ class Function(TimeStampedModel):
     def __str__(self):
         return self.name
 
-    def __str__(self):
-        return self.__str__()
-
 
 class Payroll(TimeStampedModel):
     start = models.DateField(_("Start date"))
     end = models.DateField(_("End date"))
     set_as_exported = models.BooleanField(_("Set as exported"), default=True)
-    include_already_exported = models.BooleanField(
-        _("Include already exported sessions"), default=False
-    )
+    include_already_exported = models.BooleanField(_("Include already exported sessions"), default=False)
     add_details = models.BooleanField(_("Add details"), default=False)
     csv_file = models.FileField(upload_to="payroll", blank=True, null=True)
     exported_by = models.ForeignKey(
@@ -67,6 +58,9 @@ class Payroll(TimeStampedModel):
         filelike = StringIO()
         get_payroll_csv(self, filelike)
         self.csv_file.save(
-            "{start}_{end}.csv".format(start=self.start, end=self.end),
+            f"{self.start}_{self.end}.csv",
             ContentFile(filelike.getvalue()),
         )
+
+    def __str__(self):
+        return f"{self.start} - {self.end}"
