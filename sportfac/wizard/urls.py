0598d2b0076e7b499bfd131bab50f72d100017da
Gregory Favre <greg@beyondthewall.ch>
2024-10-03 17:49:20 +0200

    account edition in new wizard

--- sportfac/wizard/urls.py
+++ sportfac/wizard/urls.py
@@ -1,19 +1,25 @@
 from django.urls import path
 
-from .views import ChildrenStepView
+from profiles.views import WizardFamilyUserUpdateView
+
+from .views import ChildrenStepView, EntryPointView
 
 
 app_name = "wizard"
 step_view_mapping = {
+    "user-update": WizardFamilyUserUpdateView,
+    "user-create": WizardFamilyUserUpdateView,
     "children": ChildrenStepView,
+    "activities": ChildrenStepView,
 }
 
 
 # URL patterns
 urlpatterns = [
+    path("", lambda request: EntryPointView.as_view()(request), name="entry_point"),
     path(
         "steps/<slug:step_slug>/",
         lambda request, step_slug: step_view_mapping[step_slug].as_view()(request),
-        name="registration_step",
+        name="step",
     ),
 ]
