787611f7ef2b99c80e7c14637589b724407c7f83
Gregory Favre <greg@beyondthewall.ch>
2024-10-08 10:21:55 +0200

    refactor

--- sportfac/wizard/admin.py
+++ sportfac/wizard/admin.py
@@ -7,5 +7,9 @@ from .models import WizardStep
 
 @admin.register(WizardStep)
 class WizardStepAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ("title", "position", "is_required")
-    list_display_links = ("title",)
+    list_display = (
+        "slug",
+        "title",
+        "position",
+    )
+    list_display_links = ("slug",)
