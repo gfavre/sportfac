3e0906124cfed226f2d8a04a57dd049d907cc614
Gregory Favre <greg@beyondthewall.ch>
2025-04-25 12:58:45 +0100

    remove comment

--- sportfac/activities/signals.py
+++ sportfac/activities/signals.py
@@ -2,7 +2,7 @@ from django.core.cache import cache
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from .models import Activity, Course  # adapte les imports si besoin
+from .models import Activity, Course
 
 
 @receiver([post_save, post_delete], sender=Course)
