6163c8c998e511f1b4918bc74fdcdebb9eac96dc
Gregory Favre <greg@beyondthewall.ch>
2025-08-18 15:07:00 +0200

    log to stdout

--- sportfac/sportfac/settings/base.py
+++ sportfac/sportfac/settings/base.py
@@ -279,6 +279,7 @@ LOGGING = {
         "console": {
             "level": "INFO",
             "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
         },
     },
     "loggers": {
