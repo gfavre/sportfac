1c3154a71d7a466187372e20cf5a1f073f2fe9e0
Gregory Favre <greg@beyondthewall.ch>
2026-01-22 09:23:24 +0100

    fix

--- sportfac/mailer/views.py
+++ sportfac/mailer/views.py
@@ -343,3 +343,9 @@ class MailCourseInstructorsView(ParticipantsBaseMixin, TemplatedEmailMixin, Canc
                 for bcc_user in bcc_list
             ],
         )
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        return self.form_invalid(form)
