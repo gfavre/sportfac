6e8699e4fe48ad85e9d56177e61787e4c3db4385
Gregory Favre <greg@beyondthewall.ch>
2026-05-18 15:53:23 +0200

    fix - adapt to new config possibility

--- sportfac/activities/forms.py
+++ sportfac/activities/forms.py
@@ -202,7 +202,7 @@ class CourseForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.pop_initial()
         self.fields["local_city_override"].help_text = _("If empty will use: %s") % ", ".join(
-            settings.KEPCHUP_LOCAL_ZIPCODES
+            " ".join(entry) if isinstance(entry, (list, tuple)) else entry for entry in settings.KEPCHUP_LOCAL_ZIPCODES
         )
         if user and user.is_restricted_manager:
             self.fields["activity"].queryset = user.managed_activities.all()
