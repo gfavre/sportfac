cef6ae2221eb38634946c5500dafe9159ec6c1e1
Gregory Favre <greg@beyondthewall.ch>
2026-07-20 12:09:20 +0200

    fix import of teachers

--- sportfac/schools/utils.py
+++ sportfac/schools/utils.py
@@ -1,22 +1,28 @@
 import re
 
-from django.db.models import Max, Min
+from django.db.models import Max
+from django.db.models import Min
 from django.utils.translation import gettext as _
-
 from openpyxl import load_workbook
+
 from profiles.models import SchoolYear
 
 from .models import Teacher
 
 
-TEACHER_MANDATORY_FIELDS = ("Numéro SPEV", "Nom", "Prénom", "Maîtrises")
-TEACHER_IMPORT_TO_FIELD = {
-    "ID LAGAPEO": "number",
-    "Numéro SPEV": "number",
-    "Nom": "last_name",
-    "Prénom": "first_name",
-    "Courriel 1": "email",
+TEACHER_CORRESPONDANCE_DICT = {
+    "number": ["Numéro SPEV", "ID LAGAPEO"],
+    "last_name": ["Nom"],
+    "first_name": ["Prénom"],
+    "email": ["Courriel 1"],
+    "classes": ["Maîtrises", "Années"],
 }
+TEACHER_MANDATORY_FIELDS = ("first_name", "last_name", "classes")
+TEACHER_COL_NAME_TO_FIELD = {}
+for _field, _col_names in TEACHER_CORRESPONDANCE_DICT.items():
+    for _col_name in _col_names:
+        TEACHER_COL_NAME_TO_FIELD.setdefault(_col_name, _field)
+
 YEARS_MINMAX = SchoolYear.objects.all().aggregate(Min("year"), Max("year"))
 ALL_YEARS = list(range(YEARS_MINMAX["year__min"], YEARS_MINMAX["year__max"] + 1))
 
@@ -26,8 +32,9 @@ def load_teachers(filelike, building=None):  # noqa: CCR001
         xls_book = load_workbook(filelike)
         sheet = xls_book.active
         header_row = [cell.value for cell in sheet[1]]
-        if not all(key in header_row for key in TEACHER_MANDATORY_FIELDS):
-            raise ValueError(_("All these fields are mandatory: %s") % str(TEACHER_MANDATORY_FIELDS))
+        for field in TEACHER_MANDATORY_FIELDS:
+            if not any(col_name in header_row for col_name in TEACHER_CORRESPONDANCE_DICT[field]):
+                raise ValueError(_("Missing mandatory field: %s") % field)
     except (ValueError, KeyError) as exc:
         raise ValueError(_("File format is unreadable")) from exc
 
@@ -38,17 +45,31 @@ def load_teachers(filelike, building=None):  # noqa: CCR001
         values = dict(zip(header_row, row))
         translated = {}
         for key, val in values.items():
-            if key in TEACHER_IMPORT_TO_FIELD:
-                translated[TEACHER_IMPORT_TO_FIELD[key]] = val
+            field = TEACHER_COL_NAME_TO_FIELD.get(key)
+            if field and field != "classes":
+                translated[field] = val
 
-        number = translated.pop("number")
-
-        classes = values.get("Maîtrises", "")
+        classes = None
+        for col_name in TEACHER_CORRESPONDANCE_DICT["classes"]:
+            if values.get(col_name):
+                classes = values[col_name]
+                break
         if not classes:
             nb_skipped += 1
             continue
 
-        teacher, created = Teacher.objects.update_or_create(number=number, defaults=translated)
+        number = translated.pop("number", None)
+        if number:
+            teacher, created = Teacher.objects.update_or_create(number=number, defaults=translated)
+        elif translated.get("first_name") and translated.get("last_name"):
+            teacher, created = Teacher.objects.update_or_create(
+                first_name=translated["first_name"],
+                last_name=translated["last_name"],
+                defaults=translated,
+            )
+        else:
+            nb_skipped += 1
+            continue
         if building:
             teacher.buildings.add(building)
         teacher.years.clear()
